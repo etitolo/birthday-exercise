9acd7bf6cf2ecfe6f692cfe984133c8f921fd820
etitolo <etitolo@wsgc.com>
2016-03-31 12:46:06 -0700

    moved form into JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var firstNameElement = document.querySelector(".firstName");
-var secondNameElement = document.querySelector(".lastName");
-var errorFirstName = document.querySelector(".firstNameError");
-var errorSecondName = document.querySelector(".secondNameError");
+var firstNameElement = document.querySelectorAll(".firstName")[0];
+var secondNameElement = document.querySelectorAll(".lastName")[0];
+var errorFirstName = document.querySelectorAll(".firstNameError")[0];
+var errorSecondName = document.querySelectorAll(".secondNameError")[0];
 
 function yourName() {
     var firstName = firstNameElement.value;
@@ -18,8 +18,7 @@ function yourName() {
     } else {
         errorSecondName.innerHTML = "";
     }
-    
-    document.getElementById("nameValues").innerHTML = firstName + " " + lastName;
+    document.querySelector(".nameValues").innerHTML = firstName + " " + lastName;
 }
 
 var contentContainer = document.querySelectorAll(".content")[0];
@@ -37,7 +36,6 @@ contentContainer.addEventListener("click", function(evt) {
       break;
   }
                                                          }, false);
-
 function reset() {
     errorFirstName.innerHTML = "";
     errorSecondName.innerHTML = "";
@@ -46,9 +44,25 @@ function reset() {
     secondNameElement.value = "";
 }
 
-var nameWrapper = document.querySelector(".nameWrapper");
+function nameForm() {
+    var buildNameWrapper = document.createElement("div");
+    buildNameWrapper.setAttribute('class', 'nameWrapper');
+    contentContainer.appendChild(buildNameWrapper);
+    
+    var nameWrapper = document.querySelector(".nameWrapper");
+    
+    nameWrapper.insertAdjacentHTML('beforeend', '<p class=\"error firstNameError\"></p><p class=\"error secondNameError\"></p>');
+
+    nameWrapper.insertAdjacentHTML("beforeend","<label>First* <input type=\"text\" class=\"firstName\"></label><label>Last* <input type=\"text\" class=\"lastName\"></label>");
+    
+    var addButton = "<button class=\"addName\">Add</button>";
+    nameWrapper.insertAdjacentHTML("beforeend","<button class=\"submitName\">Submit</button>" + addButton + "<button class=\"resetForm\">Reset</button>" );
+    
+    nameWrapper.insertAdjacentHTML("beforeend","<p class=\"nameValues\"></p>");
+    
+} nameForm();
+
 function addName(){
-    var dup = nameWrapper.cloneNode(true);
-    nameWrapper.parentNode.appendChild(dup); 
+  nameForm();
 }
 
