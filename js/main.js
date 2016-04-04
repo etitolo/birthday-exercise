697cebdb1512aa0037e23f72c4dfdfa84acce0a0
etitolo <etitolo@wsgc.com>
2016-04-04 14:12:10 -0700

    Gautam's changes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-var bday = bday || {};
-var contentContainer = document.querySelectorAll(".content")[0];
 
-bday.nameForm = function(){
+var formContainer = document.querySelector(".formContainer");
+
+
+function injectForm(containerNode) {
     var buildNameWrapper = document.createElement("div");
-    var nameCount = document.querySelectorAll(".nameWrapper").length;
-    var addButton = nameCount === 0 ? "<button class=\"addName\">Add</button>" : "";
     buildNameWrapper.setAttribute("class", "nameWrapper");
-    buildNameWrapper.innerHTML = "<p class=\"error firstNameError\"></p><p class=\"error secondNameError\"></p><label>First* <input type=\"text\" class=\"firstName\"></label><label>Last* <input type=\"text\" class=\"lastName\"></label><button class=\"submitName\">Submit</button>" + addButton + "<button class=\"resetForm\">Reset</button><p class=\"nameValues\"></p>";
-    contentContainer.appendChild(buildNameWrapper);
-}; bday.nameForm();
+    buildNameWrapper.innerHTML = "<p class=\"error firstNameError\"></p><p class=\"error secondNameError\"></p><label>First* <input type=\"text\" class=\"firstName\"></label><label>Last* <input type=\"text\" class=\"lastName\"></label><button class=\"submitName\">Submit</button><button class=\"resetForm\">Reset</button><p class=\"nameValues\"></p>";
+    containerNode.appendChild(buildNameWrapper);
+}
 
-bday.yourName = function(){
-  var nameCount = document.querySelectorAll(".nameWrapper").length;
-  for (var i=0; i < nameCount; i++) {
-    var firstNameElement = document.querySelectorAll(".firstName")[i];
-    var secondNameElement = document.querySelectorAll(".lastName")[i];
-    var errorFirstName = document.querySelectorAll(".firstNameError")[i];
-    var errorSecondName = document.querySelectorAll(".secondNameError")[i];
-    var firstName = firstNameElement.value;
-    var lastName = secondNameElement.value;    
+function reset(parentNode) {
+  var errors = parentNode.querySelectorAll(".error"),
+      inputFileds = parentNode.querySelectorAll("input"),
+      count;
+  for (count = 0; count < errors.length; count++ ) {
+    errors[count].innerHTML = "";
+  }
+  for (count = 0; count < inputFileds.length; count++ ) {
+    inputFileds[count].value = "";
+  }
+}
+
+function handleSubmit(parentNode) {
+    var firstNameElement = parentNode.querySelector(".firstName"),
+        secondNameElement = parentNode.querySelector(".lastName"),
+        errorFirstName = parentNode.querySelector(".firstNameError"),
+        errorSecondName = parentNode.querySelector(".secondNameError"),
+        firstName = firstNameElement.value,
+        lastName = secondNameElement.value;    
     if (!firstName){
         errorFirstName.innerHTML = "Please enter your first name.";
     } else {
@@ -29,37 +38,43 @@ bday.yourName = function(){
     } else {
         errorSecondName.innerHTML = "";
     }
-    document.querySelectorAll(".nameValues")[i].innerHTML = firstName + " " + lastName;
-  }
-};
+    parentNode.querySelector(".nameValues").innerHTML = firstName + " " + lastName;
+}
 
-bday.buttonController = (function(){
-  contentContainer.addEventListener("click", function(evt){ 
-    var elementSelector = evt.target.className;
-    switch(elementSelector) {
+function registerEventListners() {
+  var addButton = document.querySelector(".addName");
+
+  addButton.addEventListener("click", function(evt){
+     injectForm(formContainer);
+  });
+
+  formContainer.addEventListener("click", function(evt){
+    var targetClassName = evt.target.className,
+        parentNode = evt.target.parentElement;
+    
+    switch(targetClassName) {
       case "submitName":
-        bday.yourName();
+        handleSubmit(parentNode);
         break;
       case "resetForm":
-        bday.reset();
+        reset(parentNode);
         break;
-      case "addName":
-        bday.nameForm();
-        break;
-    }                                                       }, false);
-})();
+    }          
+  });
+}
 
-bday.reset = function(){
-    var nameCount = document.querySelectorAll(".nameWrapper").length;
-    for (var i=0; i < nameCount; i++) {
-      var firstNameElement = document.querySelectorAll(".firstName")[i];
-      var secondNameElement = document.querySelectorAll(".lastName")[i];
-      var errorFirstName = document.querySelectorAll(".firstNameError")[i];
-      var errorSecondName = document.querySelectorAll(".secondNameError")[i];
-      errorFirstName.innerHTML = "";
-      errorSecondName.innerHTML = "";
-      document.querySelectorAll(".nameValues")[i].innerHTML = "";
-      firstNameElement.value = "";
-      secondNameElement.value = "";
-    }
-};
+function initialize() {
+  var addButton =  document.createElement("div"),
+      buttonContainer = document.querySelector(".buttonContainer");
+  addButton.innerHTML = "<button class=\"addName\">Add</button>";
+  
+  buttonContainer.appendChild(addButton);
+
+  injectForm(formContainer);
+}
+
+initialize();
+registerEventListners();
+
+
+
