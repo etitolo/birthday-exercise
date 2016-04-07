7274781ada36e3adfd2f43695a89bd8129137aaf
etitolo <etitolo@wsgc.com>
2016-04-06 17:26:46 -0700

    add save button. added set session storage. add save messaging

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var formContainer = document.querySelector(".formContainer");
 function injectForm(containerNode) {
     var buildNameWrapper = document.createElement("div");
     buildNameWrapper.setAttribute("class", "nameWrapper");
-    buildNameWrapper.innerHTML = "<div class=sidebar><input type=checkbox class=deleteCheckbox></div><div class=inputContainer><p class=\"error firstNameError\"><p class=\"error secondNameError\"></p><label>First* <input class=firstName></label><label>Last* <input class=lastName></label><button class=submitName>Submit</button> <button class=resetForm>Reset</button><p class=nameValues></p></div>";
+    buildNameWrapper.innerHTML = "<div class='sidebar'><input type='checkbox' class='deleteCheckbox'></div><div class='inputContainer'><p class='error firstNameError'><p class='error secondNameError'></p><label>First* <input type='text' class='firstName'></label><label>Last* <input type='text' class='lastName'></label><button class='submitName'>Submit</button><button class='resetForm'>Reset</button><p class='nameValues'></p></div>";
     containerNode.appendChild(buildNameWrapper);
 }
 
@@ -29,11 +29,11 @@ function deleteEntry(){
 
 function handleSubmit(parentNode){
     var firstNameElement = parentNode.querySelector(".firstName"),
-        secondNameElement = parentNode.querySelector(".lastName"),
+        lastNameElement = parentNode.querySelector(".lastName"),
         errorFirstName = parentNode.querySelector(".firstNameError"),
         errorSecondName = parentNode.querySelector(".secondNameError"),
         firstName = firstNameElement.value,
-        lastName = secondNameElement.value;
+        lastName = lastNameElement.value;
     if (!firstName){
         errorFirstName.innerHTML = "Please enter your first name.";
     } else {
@@ -47,9 +47,35 @@ function handleSubmit(parentNode){
     parentNode.querySelector(".nameValues").innerHTML = firstName + " " + lastName;
 }
 
+function setSessionStorage(){
+  var firstNameElement = formContainer.querySelectorAll(".firstName"),
+      lastNameElement = formContainer.querySelectorAll(".lastName"),
+      firstNameArray = [],
+      lastNameArray = [];
+  if (window.sessionStorage) {
+    for (var count=0; count < firstNameElement.length; count++) {
+      var firstName = firstNameElement[count].value;
+      var lastName = lastNameElement[count].value;
+      firstNameArray.push(firstName);
+      lastNameArray.push(lastName);
+      window.sessionStorage.setItem('firstName', JSON.stringify(firstNameArray));
+      window.sessionStorage.setItem('lastName', JSON.stringify(lastNameArray));
+    }
+    messaging();
+  } else {
+    alert("Session storage is not available.");
+  }
+}
+
+function messaging() {
+  document.querySelector(".messaging").classList.remove("hide");
+  window.setTimeout(function(){document.querySelector(".messaging").classList.add("hide")},2000);
+}
+
 function registerEventListners() {
   var addButton = document.querySelector(".addName");
   var deleteButton = document.querySelector(".deleteName");
+  var saveButton = document.querySelector(".saveName")
 
   addButton.addEventListener("click", function(evt){
      injectForm(formContainer);
@@ -57,6 +83,10 @@ function registerEventListners() {
 
   deleteButton.addEventListener("click", function(evt){
      deleteEntry();
+  });
+
+  saveButton.addEventListener("click", function(evt){
+     setSessionStorage();
   });
 
   formContainer.addEventListener("click", function(evt){
@@ -76,7 +106,7 @@ function registerEventListners() {
 function initialize() {
   var addButton =  document.createElement("div"),
       buttonContainer = document.querySelector(".buttonContainer");
-  addButton.innerHTML = "<button class=\"addName\">Add</button><button class=\"deleteName\">Delete</button>";
+  addButton.innerHTML = "<button class=\"addName\">Add</button><button class=\"saveName\">Save</button><button class=\"deleteName\">Delete</button>";
   buttonContainer.appendChild(addButton);
 
   injectForm(formContainer);
@@ -84,3 +114,6 @@ function initialize() {
 
 initialize();
 registerEventListners();
+
+
+
