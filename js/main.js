38f8e4c6479287315140823e0636a46171d79991
etitolo <etitolo@wsgc.com>
2016-04-07 11:54:00 -0700

    Add getter for local storage

--- js/main.js
+++ js/main.js
@@ -17,10 +17,13 @@ function reset(parentNode) {
   for (count = 0; count < inputFileds.length; count++ ) {
     inputFileds[count].value = "";
   }
+  setLocalStorage();
 }
 
 function deleteEntry(){
     var deleteCheckbox = document.querySelectorAll(".deleteCheckbox:checked");
+    localStorage.removeItem('firstName');
+    localStorage.removeItem('lastName');
     for (var count=0; count < deleteCheckbox.length; count++) {
       var nameWrapper = deleteCheckbox[count].parentNode.parentNode;
       formContainer.removeChild(nameWrapper);
@@ -45,6 +48,7 @@ function handleSubmit(parentNode){
         errorSecondName.innerHTML = "";
     }
     parentNode.querySelector(".nameValues").innerHTML = firstName + " " + lastName;
+    setLocalStorage();
 }
 
 function setLocalStorage(){
@@ -58,8 +62,8 @@ function setLocalStorage(){
       var lastName = lastNameElement[count].value;
       firstNameArray.push(firstName);
       lastNameArray.push(lastName);
-      window.localStorage.setItem('firstName', JSON.stringify(firstNameArray));
-      window.localStorage.setItem('lastName', JSON.stringify(lastNameArray));
+      localStorage.setItem('firstName', JSON.stringify(firstNameArray));
+      localStorage.setItem('lastName', JSON.stringify(lastNameArray));
     }
     messaging();
   } else {
@@ -67,15 +71,33 @@ function setLocalStorage(){
   }
 }
 
+function getLocalStorage() {
+  var firstNameArray = JSON.parse(localStorage.getItem("firstName"));
+  var lastNameArray = JSON.parse(localStorage.getItem("lastName"));
+  var containerNode = document.querySelector(".formContainer");
+  if (firstNameArray || lastNameArray) {
+    for (var count=0; count < firstNameArray.length - 1; count++) {
+      injectForm(containerNode);
+    }
+    var firstNameInput = document.querySelectorAll(".firstName");
+    var lastNameInput = document.querySelectorAll(".lastName");
+    for (var i=0; i < firstNameArray.length; i++) {
+      firstNameInput[i].value = firstNameArray[i];
+      lastNameInput[i].value = lastNameArray[i];
+    }
+  }
+}
+
 function messaging() {
-  document.querySelector(".messaging").classList.remove("hide");
-  window.setTimeout(function(){document.querySelector(".messaging").classList.add("hide")},2000);
+  var messageing = document.querySelector(".messaging");
+  messageing.classList.remove("hide");
+  window.setTimeout(function(){messageing.classList.add("hide");},2000);
 }
 
 function registerEventListners() {
   var addButton = document.querySelector(".addName");
   var deleteButton = document.querySelector(".deleteName");
-  var saveButton = document.querySelector(".saveName")
+  var saveButton = document.querySelector(".saveName");
 
   addButton.addEventListener("click", function(evt){
      injectForm(formContainer);
@@ -91,7 +113,7 @@ function registerEventListners() {
 
   formContainer.addEventListener("click", function(evt){
     var targetClassName = evt.target.className,
-        plocalde = evt.target.parentElement;
+        parentNode = evt.target.parentElement;
     switch(targetClassName) {
       case "submitName":
         handleSubmit(parentNode);
@@ -108,8 +130,8 @@ function initialize() {
       buttonContainer = document.querySelector(".buttonContainer");
   addButton.innerHTML = "<button class=\"addName\">Add</button><button class=\"saveName\">Save</button><button class=\"deleteName\">Delete</button>";
   buttonContainer.appendChild(addButton);
-
   injectForm(formContainer);
+  getLocalStorage();
 }
 
 initialize();
